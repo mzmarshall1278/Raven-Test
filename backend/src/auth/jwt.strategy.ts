4ac1b61b32ba59b48c6dbb448e00efcc82549399
mzmarshall1278 <mzmarshall1278@gmail.com>
2025-04-25 14:29:18 +0100

    create jwt strategy for auth

---
+++ backend/src/auth/jwt.strategy.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthGuard } from '@nestjs/passport';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'jwtsecret',
+    });
+  }
+
+  async validate(payload: any) {
+    return { id: payload.sub, email: payload.email };
+  }
+}
+
+export class JwtAuthGuard extends AuthGuard('jwt') {}
